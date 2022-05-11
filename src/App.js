13fb1596857ea74173c6aea6f2040855e36048b8
FisayoH <101359278+FisayoH@users.noreply.github.com>
2022-05-11 13:49:00 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>You have 3 Todos</h3>
+      <h4>task 1</h4>
+      <h4>task 2</h4>
+      <h4>task 3</h4>
     </div>
   );
 }
